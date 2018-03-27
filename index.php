aebb3a550013e1f902bf91e985fe27a9ef02d2bd
Olav <olavrui@gmail.com>
2018-03-27 14:49:58 +0200

    Opprydding

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-
-require_once 'db.php';
-include 'SkirennKlasser.php';
-include 'feilhandtering.php';
-
-
+    require_once 'db.php';
+    include 'SkirennKlasser.php';
 ?>
+
 <html>
     <head>
         <title>Skirennregister</title> 
