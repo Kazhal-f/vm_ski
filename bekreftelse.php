dbcfdc110787bed1a48cda2edaed453ea90023ad
Olav <olavrui@gmail.com>
2018-03-27 14:49:37 +0200

    Regex på bekreftelse

--- bekreftelse.php
+++ bekreftelse.php
@@ -1,7 +1,6 @@
 <?php
         session_start();
         require_once 'db.php';
-        include "SkirennKlasser.php";
         ?>
 <!DOCTYPE html>
 <html>
@@ -19,12 +18,29 @@
             $utover = unserialize($_SESSION["utover"]);
             $utover->set_nasjonalitet($_REQUEST["nasjonalitet"]);
             $ovelseId = $_REQUEST["ovelseId"];
-
-            mysqli_query($db, "INSERT INTO Person(Fornavn, Etternavn, Adresse, PostNr, Poststed, TelefonNr)
+            
+            //REGEX
+            $OK = true;
+            
+            if (!preg_match('/^[1-9]{1,1000}$/', $ovelseId)) {
+                echo "Feil format på ØvelsesId. Skriv kun tallet på den øvelsen du ønsker å endre.</br>";
+                $OK = false;
+            }
+            
+            if (! $utover->valider_nasjonalitet($utover->get_nasjonalitet())) {
+                    echo "Feil format på nasjonalitet.</br>";
+                    $OK = false;
+            }
+            
+            if ($OK) {
+                mysqli_query($db, "INSERT INTO Person(Fornavn, Etternavn, Adresse, PostNr, Poststed, TelefonNr)
                     VALUES ('" . $utover->get_fornavn() . "','" . $utover->get_etternavn() . "','" . $utover->get_adresse() . "','" . $utover->get_postnr() . "',
                     '" . $utover->get_poststed() . "','" . $utover->get_telefonnr() . "') ");
             
-            mysqli_query($db, "INSERT INTO Utover (personId, Nasjonalitet, ovelseId) VALUES ( LAST_INSERT_ID(), '".$utover->get_nasjonalitet()."', '".$ovelseId."')");
+                mysqli_query($db, "INSERT INTO Utover (personId, Nasjonalitet, ovelseId) VALUES ( LAST_INSERT_ID(), '".$utover->get_nasjonalitet()."', '".$ovelseId."')");
+            }
+
+            
 
             echo "<h3>Du ble registrert med informasjonen:</h3>";
             //TESTE OUTPUT
@@ -41,13 +57,25 @@
             $publikum = unserialize($_SESSION["publikum"]);
             $publikum->set_billettype($_REQUEST["billettype"]);
             $ovelseId = $_REQUEST["ovelseId"];
-
-            mysqli_query($db, "INSERT INTO Person(Fornavn, Etternavn, Adresse, PostNr, Poststed, TelefonNr)
+            
+            //REGEX
+            $OK = true;
+            
+            if (!preg_match('/^[1-9]{1,1000}$/', $ovelseId)) {
+                echo "Feil format på ØvelsesId. Skriv kun tallet på den øvelsen du ønsker å endre.</br>";
+                $OK = false;
+            }
+            
+            if ($OK) {
+                mysqli_query($db, "INSERT INTO Person(Fornavn, Etternavn, Adresse, PostNr, Poststed, TelefonNr)
                     VALUES ('" . $publikum->get_fornavn() . "','" . $publikum->get_etternavn() . "','" . $publikum->get_adresse() . "','" . $publikum->get_postnr() . "',
                     '" . $publikum->get_poststed() . "','" . $publikum->get_telefonnr() . "') ");
             
-            mysqli_query($db, "INSERT INTO publikum (personId, BilettType, ovelseId) VALUES ( LAST_INSERT_ID(), '".$publikum->get_billettype()."', '".$ovelseId."' )" );
+                mysqli_query($db, "INSERT INTO publikum (personId, BilettType, ovelseId) VALUES ( LAST_INSERT_ID(), '".$publikum->get_billettype()."', '".$ovelseId."' )" );
+            }
+
             
+            //TESTE OUTPUT
             echo $publikum->get_fornavn() . "<br>";
             echo $publikum->get_etternavn() . "<br>";
             echo $publikum->get_adresse() . "<br>";
