2e04ec73709ed48563ac1ec2ff94f0044e79f7f7
Olav <olavrui@gmail.com>
2018-03-27 13:18:52 +0200

    Kodeformatering og feilmelding oppdatert

--- ovelse.php
+++ ovelse.php
@@ -1,8 +1,8 @@
 <?php
-        session_start();
-        require_once 'db.php';
-        include "SkirennKlasser.php";
-        ?>
+    session_start();
+    require_once 'db.php';
+    include "SkirennKlasser.php";
+?>
 <html>
     <head>
         <title>Skirennregister</title> 
@@ -99,57 +99,52 @@ echo '</table>';
 //LEGG TIL
 if (isset($_REQUEST['legg'])) {
     $ovelse = new ovelse;
-            $ovelse->set_type($_REQUEST['type']);
-            $ovelse->set_dato($_REQUEST['dato']);
-            $ovelse->set_tid($_REQUEST['tid']);
-            $ovelse->set_sted($_REQUEST['sted']);
+        $ovelse->set_type($_REQUEST['type']);
+        $ovelse->set_dato($_REQUEST['dato']);
+        $ovelse->set_tid($_REQUEST['tid']);
+        $ovelse->set_sted($_REQUEST['sted']);
     
     $OK = true;
     
     //REGEX LEGG TIL
     if(! $ovelse->valider_type($ovelse->get_type())) {
-            echo "Brukt riktige tegn!<br>";
-            $OK = false;
-        } 
+        echo "Feil format på type. Riktig format er for eks 10mila.<br>";
+        $OK = false;
+    } 
 
         
     if(! $ovelse->valider_dato($ovelse->get_dato())) {
-            echo "Feil format på dato. Eks 23.01.2018<br>";
-            $OK = false;
-        } 
+        echo "Feil format på dato. Riktig format er for eks 23.01.2018<br>";
+        $OK = false;
+    } 
 
     
     if(! $ovelse->valider_tid($ovelse->get_tid())) {
-            echo "Feil format på tid. For eks 12:00<br>";
-            $OK = false;
-        } 
+        echo "Feil format på tid. Riktig format er for eks 12:00<br>";
+        $OK = false;
+    } 
 
     
     if(! $ovelse->valider_sted($ovelse->get_sted())) {
-            echo "Feil format på sted<br>";
-            $OK = false;
-        } 
+        echo "Feil format på sted. Riktig format er for eks Oslo.<br>";
+        $OK = false;
+    } 
 
-        if($OK) {
+    if($OK) {
         mysqli_query($db, "INSERT INTO ovelse(ovelse, dato, tid, sted)
-                    VALUES ('" . $ovelse->get_type() . "','" . $ovelse->get_dato() . "','" . $ovelse->get_tid() . "','" . $ovelse->get_sted() . "') ");
+        VALUES ('" . $ovelse->get_type() . "','" . $ovelse->get_dato() . "','" . $ovelse->get_tid() . "','" . $ovelse->get_sted() . "') ");
         $resultat = $db->query($sql);
-        }
-    /*if (!$ovelse->)
-            
-            mysqli_query($db, "INSERT INTO ovelse(ovelse, dato, tid, sted)
-                    VALUES ('" . $ovelse->get_ovelse() . "','" . $ovelse->get_dato() . "','" . $ovelse->get_tid() . "','" . $ovelse->get_sted() . "') ");
- */         
+    }       
 }
 
 //UPDATE
 if (isset($_REQUEST['endre'])) {
     
     $ovelse = new ovelse;
-            $ovelse->set_type($_REQUEST['type']);
-            $ovelse->set_dato($_REQUEST['dato']);
-            $ovelse->set_tid($_REQUEST['tid']);
-            $ovelse->set_sted($_REQUEST['sted']);
+        $ovelse->set_type($_REQUEST['type']);
+        $ovelse->set_dato($_REQUEST['dato']);
+        $ovelse->set_tid($_REQUEST['tid']);
+        $ovelse->set_sted($_REQUEST['sted']);
             
     $id = $_REQUEST["ovelsesId"];
 
@@ -161,24 +156,24 @@ if (isset($_REQUEST['endre'])) {
     $OK = true;
     
     if(! $ovelse->valider_type($ovelse->get_type())) {
-            echo "Feil format på type. Riktig format er for eks 10mila.<br>";
-            $OK = false;
-        } 
+        echo "Feil format på type. Riktig format er for eks 10mila.<br>";
+        $OK = false;
+    } 
         
     if(! $ovelse->valider_dato($ovelse->get_dato())) {
-            echo "Feil format på dato. Riktig format er for eks 23.01.2018<br>";
-            $OK = false;
-        } 
+        echo "Feil format på dato. Riktig format er for eks 23.01.2018<br>";
+        $OK = false;
+    } 
 
     if(! $ovelse->valider_tid($ovelse->get_tid())) {
-            echo "Feil format på tid. Riktig format er for eks 12:00<br>";
-            $OK = false;
-        } 
+        echo "Feil format på tid. Riktig format er for eks 12:00<br>";
+        $OK = false;
+    } 
 
     if(! $ovelse->valider_sted($ovelse->get_sted())) {
-            echo "Feil format på sted. Riktig format er for eks Oslo.<br>";
-            $OK = false;
-        } 
+        echo "Feil format på sted. Riktig format er for eks Oslo.<br>";
+        $OK = false;
+    } 
 
         
     if($OK) {
