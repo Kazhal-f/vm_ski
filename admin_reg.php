bf2a5d43f353362281adb29a88fa990fab69d622
Olav <olavrui@gmail.com>
2018-04-11 19:37:28 +0200

    Admin reg

--- admin_reg.php
+++ admin_reg.php
@@ -1,34 +1,47 @@
-<html>
-    <head>
-        <title>Skirennregister</title> 
-    </head>
-    <body>
-        <h1>Skirennregister</h1>
-        <h3>Vennligst skriv inn hvem du ønsker å registrere:</h3>
-        <form action="" method="post">
-            <table>
-                <tr>
-                    <td>Navn:</td>
-                    <td><input type="text" name="navn"></td>
-                </tr>
-                <tr>
-                    <td>Brukernavn:</td>
-                    <td><input type="text" name="brukernavn"></td>
-                </tr>
-                <tr>
-                    <td>Passord:</td>
-                    <td><input type="password" name="passord"></td>
-                </tr>
-                <tr>
-                    <td><input type="submit" name="registrer" value="Registrer"></td>
-                </tr>
-            </table>
-        </form>
-    </body>  
-</html>
 <?php
 session_start();
+require_once 'db.php';
 
+//LoggInn Feilet
+If (!$_SESSION["loggetInn"]){
+    echo "Du  er ikke logget på ! Trykk her for å logge på :";
+    echo "<a href='logginn.php'>Tilbake</a>";
+    die(); 
+}
+
+//Logget Inn
+if ($_SESSION["loggetInn"]) {
+    echo '<html>
+            <head>
+                <title>Skirennregister</title> 
+            </head>
+            <body>
+                <h1>Administrator</h1>
+                <h3>Vennligst skriv inn hvem du ønsker å registrere:</h3>
+                <form action="" method="post">
+                    <table>
+                        <tr>
+                            <td>Navn:</td>
+                            <td><input type="text" name="navn"></td>
+                        </tr>
+                        <tr>
+                            <td>Brukernavn:</td>
+                            <td><input type="text" name="brukernavn"></td>
+                        </tr>
+                        <tr>
+                            <td>Passord:</td>
+                            <td><input type="password" name="passord"></td>
+                        </tr>
+                        <tr>
+                            <td><input type="submit" name="registrer" value="Registrer"></td>
+                        </tr>
+                    </table>
+                </form>
+            </body>  
+        </html>';
+}
+
+//Registrering av brukere
 if(isset($_POST["registrer"])) {
     $brukernavn = mysqli_real_escape_string($db, $_POST["brukernavn"]);
     $passord = md5($_POST["passord"]);
@@ -42,10 +55,5 @@ if(isset($_POST["registrer"])) {
         echo "Feilet";
     }
 }
-
-
-
-
-
 ?>
 
