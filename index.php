f505b34b12b0db5962e1f1936c9a018a13e8b80a
Olav <olavrui@gmail.com>
2018-04-12 13:18:44 +0200

    JS regEx index publikum

--- index.php
+++ index.php
@@ -6,35 +6,102 @@
 <html>
     <head>
         <title>Skirennregister</title> 
+        <script type="text/javascript">
+            function valider_navn() {
+                regEx = /^[a-åA-Å.\-]{3,20}$/;
+                OK = regEx.test(document.publikum.fornavn.value);
+            if (!OK) {
+                document.getElementById("feilFornavn").innerHTML="Feil format i fornavnet";
+                return false;
+            }
+            document.getElementById("feilFornavn").innerHTML="";
+            return true; }
+        
+            function valider_etternavn() {
+                regEx = /^[a-åA-Å.\-]{3,30}$/;
+                OK = regEx.test(document.publikum.etternavn.value);
+            if (!OK) {
+                document.getElementById("feilEtternavn").innerHTML="Feil format i etternavnet";
+                return false;
+            }
+            document.getElementById("feilEtternavn").innerHTML="";
+            return true; }
+        
+            function valider_adresse() {
+                regEx = /^[a-åA-Å0-9.\-]{3,30}$/;
+                OK = regEx.test(document.publikum.adresse.value);
+            if (!OK) {
+                document.getElementById("feilAdresse").innerHTML="Feil format i adressen";
+                return false;
+            }
+            document.getElementById("feilAdresse").innerHTML="";
+            return true; }
+        
+            function valider_postnr() {
+                regEx = /^[0-9]{4}$/;
+                OK = regEx.test(document.publikum.postnr.value);
+            if (!OK) {
+                document.getElementById("feilPostnr").innerHTML="Feil i postnummeret";
+                return false;
+            }
+            document.getElementById("feilPostnr").innerHTML="";
+            return true; }
+        
+            function valider_poststed() {
+                regEx = /^[a-åA-Å0-9.\-]{3,30}$/;
+                OK = regEx.test(document.publikum.poststed.value);
+            if (!OK) {
+                document.getElementById("feilPoststed").innerHTML="Feil format i poststed";
+                return false;
+            }
+            document.getElementById("feilPoststed").innerHTML="";
+            return true; }
+        
+            function valider_tlf() {
+                regEx = /^[0-9]{8}$/;
+                OK = regEx.test(document.publikum.tlf.value);
+            if (!OK) {
+                document.getElementById("feilTlf").innerHTML="Feil format. Kun norske numre";
+                return false;
+            }
+            document.getElementById("feilTlf").innerHTML="";
+            return true; }
+        </script>
     </head>
     <body>
         <h1>Skirennregister</h1>
         <h3>Registrer deg som publikum her:</h3>
-        <form action="utover_publikum.php" method="post">
+        <form action="utover_publikum.php" method="post" name="publikum">
             <table>
                 <tr>
                     <td>Fornavn:</td>
-                    <td><input type="text" name="fornavn"></td>
+                    <td><input type="text" name="fornavn" onchange="valider_navn()"></td>
+                    <td><div id="feilFornavn">*</div></td>
                 </tr>
                 <tr>
                     <td>Etternavn:</td>
-                    <td><input type="text" name="etternavn"></td>
+                    <td><input type="text" name="etternavn" onChange="valider_etternavn()"></td>
+                    <td><div id="feilEtternavn">*</div></td>
                 </tr>
                 <tr>
                     <td>Adresse:</td>
-                    <td><input type="text" name="adresse"></td>
+                    <td><input type="text" name="adresse" onchange="valider_adresse()"></td>
+                    <td><div id="feilAdresse">*</div></td>
                 </tr>
                 <tr>
                     <td>Postnr:</td>
-                    <td><input type="text" name="postnr"></td>
+                    <td><input type="text" name="postnr" onchange="valider_postnr()"></td>
+                    <td><div id="feilPostnr">*</div></td>
                 </tr>
                 <tr>
                     <td>Poststed:</td>
-                    <td><input type="text" name="poststed"></td>
+                    <td><input type="text" name="poststed" onchange="valider_poststed()"></td>
+                    <td><div id="feilPoststed">*</div></td>
                 </tr>
                 <tr>
                     <td>Telefon:</td>
-                    <td><input type="text" name="tlf"></td>
+                    <td><input type="text" name="tlf" onchange="valider_tlf()"></td>
+                    <td><div id="feilTlf">*</div></td>
                 </tr>
                 <tr>
                     <td><input type="submit" name="registrer" value="Registrer"></td>
@@ -65,7 +132,6 @@
                 </tr>
             </table>
         </form>
-        <button onclick="window.location.href='logginn.php'">Administrer brukere her</button>
     </body>  
 </html>
 
@@ -89,7 +155,7 @@ if (isset($_POST["logg_inn_bruker"])) {
         }
     if($ok) {
         $_SESSION["loggetInn"]=true;
-        header("location: ./ovelse.php");
+        header("location: ./logginn.php");
         }
     if (!$ok) {
         echo "</br>Passordet stemte ikke med brukernavnet du skrev inn!";
