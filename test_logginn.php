e5fd82d5184f7fea0a5d95644a03fea9024b93d1
Olav <olavrui@gmail.com>
2018-04-14 10:48:18 +0200

    Logginn feil

---
+++ test_logginn.php
@@ -0,0 +1,10 @@
+<?php
+//LoggInn Feilet
+If (!$_SESSION["loggetInn"]){
+    echo "Du  er ikke logget på ! Trykk her for å logge på :";
+    echo "<a href='index.php'>Tilbake</a>";
+    die(); 
+}
+
+?>
+
