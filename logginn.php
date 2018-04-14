2434d9bf8d236b1978a51309d05ac92e5fe9bdb2
Olav <olavrui@gmail.com>
2018-04-14 10:48:43 +0200

    Logg inn

--- logginn.php
+++ logginn.php
@@ -1,45 +1,17 @@
-<h3>Vennligst logg inn for å gjøre admin-endringer:</h3>
-        <form action="" method="post">
-            <table>
-                <tr>
-                    <td>Brukernavn:</td>
-                    <td><input type="text" name="brukernavn"></td>
-                </tr>
-                <tr>
-                    <td>Passord:</td>
-                    <td><input type="password" name="passord"></td>
-                </tr>
-                <tr>
-                    <td><input type="submit" name="logg_inn" value="Logg Inn"></td>
-                </tr>
-            </table>
-        </form>
 <?php
 session_start();
 require_once 'db.php';
-if (isset($_POST["logg_inn"])) {
-    $passord = md5($_POST["passord"]);
-    $sjekk_passord=$passord;
-    $brukernavn = $_POST["brukernavn"];
-    $sql = "SELECT passord FROM brukere WHERE brukernavn ='$brukernavn'";
-    $resultat = $db->query($sql);
-    $_SESSION["loggetInn"]=false;
-    
-    $ok = false;
-    if($db->affected_rows >=1) {
-        $rad = $resultat->fetch_object();
-        $passordHash = $rad->passord;
-        if ($passordHash == $passord) {
-           $ok = true;
-        }
-    if($ok) {
-        $_SESSION["loggetInn"]=true;
-        header("location: ./admin_reg.php");
-        }
-    if (!$ok) {
-        echo "Passordet stemte ikke med brukernavnet du skrev inn!";
-    }
-    }
-}
+include 'test_logginn.php';
 ?>
-
+<html>
+    <head>
+        <title>Skirennregister</title> 
+    </head>
+    <body>
+        <h3>Vennligst velg:</h3>
+        <button onClick="window.location.href='ovelse.php'">Øvelse</button>
+        <button onClick="window.location.href='utover.php'">Utøver</button>
+        <button onClick="window.location.href='admin_reg.php'">Brukeradministrasjon</button></br>
+        </br><button onClick="window.location.href='logg_out.php'">Logg ut</button>
+    </body>
+</html>
